c82e703d84932cb910c5b751556b6acbe4e43589
victor vinci <victorvinci@protonmail.com>
2019-05-30 21:21:12 +0200

    add logic to make basic click and drag

--- clickAndDrag.js
+++ clickAndDrag.js
@@ -3,15 +3,29 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', () => {
-  
-})
+slider.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
 slider.addEventListener('mouseleave', () => {
-  
-})
+  isDown = false;
+  slider.classList.remove('active');
+});
+
 slider.addEventListener('mouseup', () => {
-  
-})
-slider.addEventListener('mousemove', () => {
-  
-})
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return; //stop function
+  e.preventDefault();
+  e.target.click();
+  const x = e.pageX - slider.offsetLeft;
+  const moveX = x - startX;
+  slider.scrollLeft = scrollLeft - moveX;
+});
