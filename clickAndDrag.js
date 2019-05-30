a93bb59469507b44cadf699863a7535e9941194a
victor vinci <victorvinci@protonmail.com>
2019-05-30 20:30:19 +0200

    create variables and setup event listeners

---
+++ clickAndDrag.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+  
+})
+slider.addEventListener('mouseleave', () => {
+  
+})
+slider.addEventListener('mouseup', () => {
+  
+})
+slider.addEventListener('mousemove', () => {
+  
+})
